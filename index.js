cca84204c3f62c419c2a126139f0467dd3fc52c8
Jesus Hernandez <jesushernandez@Jesuss-MacBook-Pro-2.local>
2023-11-09 12:40:05 -0500

    add img from players api Source

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 //console.log("hello");
 
-const apiBaseURL = `https://fsa-puppy-bowl.herokuapp.com/api/2310-fsa-et-web-ft-sf/.`;
+const apiBaseURL = `https://fsa-puppy-bowl.herokuapp.com/api/2310-fsa-et-web-ft-sf/`;
 
-const getAllPlayers = () => {
-    const response = fetch (`$(apiBaseURL)players` );
-    console.log(response);
+const getPlayersPic = async () => {
+    const response = await fetch (`${apiBaseURL}players `);
+    const playersPic =await response.json();
+    console.log(playersPic);
+    //grab the main with querySelector
+    const main = document.querySelector(`main`);
+    //console.log(main)
+    //create an image
+   const img = document.createElement(`img`);
+    console.log(img)
+    // grab source from message
+   img.src = playersPic.data.players[0].imageUrl;
+   img.alt =`picture of player`;
+    //append it
+    main.appendChild(img);
 
 }
 
-getAllPlayers();
+
+getPlayersPic();
